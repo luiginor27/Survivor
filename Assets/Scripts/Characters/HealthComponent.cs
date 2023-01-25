0812b71c3fc908b91986503ec482dcbb78fbfe4c
Lluis Miralles <luismiralles98@gmail.com>
2023-01-25 15:03:07 +0100

    Visual feedback when hit

--- Assets/Scripts/Characters/HealthComponent.cs
+++ Assets/Scripts/Characters/HealthComponent.cs
@@ -10,18 +10,37 @@ namespace Survivor.Characters
 
         public float Health { private set; get; }
 
+        private SpriteRenderer spriteRenderer;
+        private Color originalColor;
+
         private void Start()
         {
             Health = initialHealth;
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            originalColor = spriteRenderer.color;
         }
 
         public void ReceiveDamage(float value)
         {
+            StartCoroutine(ReceiveDamageCoroutine(value));
+        }
+
+        private IEnumerator ReceiveDamageCoroutine(float value)
+        {
             Health -= value;
-            if(Health <= 0)
+
+            if (Health <= 0)
             {
                 Destroy(gameObject);
             }
+            else
+            {
+                SetColor(Color.red);
+                yield return new WaitForSeconds(0.5f);
+                SetColor(originalColor);
+            }
         }
+
+        private void SetColor(Color color) { spriteRenderer.color = color; }
     }
 }
